8bc8d1d07f88984a760c80a6f7f9a1cc4b1afcca
Krzysztof Z <krisdeveloper25@gmail.com>
2019-08-08 14:12:38 -0500

    Add missing main file.

---
+++ intel-reg-pp/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "reg_print.h"
+#include "reg_print_args.h"
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+    stREG_PRINT__DEF stReg = { 0, REG_PRINT__ID_UNKNOWN, 0, 0, 0};
+
+    printf("\nIntel Register Pretty Print (intel-reg-pp) v1.0.0\n\n");
+
+    // Determine which register to print
+    REG_PRINT_ARGS__Parse(argc, argv, &stReg);
+
+    // Now, perform the pretty-print
+    REG_PRINT__DisplayDetails(&stReg);
+
+    return 0;
+}
